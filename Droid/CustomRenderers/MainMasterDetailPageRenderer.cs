08ded8a5acb1fe07b74064814f10306b230fab98
Paul Hart <pmhart@ymail.com>
2017-02-14 12:14:20 -0600

    goes with last commit, solution from stackoverflow / xamarin forms

--- Droid/CustomRenderers/MainMasterDetailPageRenderer.cs
+++ Droid/CustomRenderers/MainMasterDetailPageRenderer.cs
@@ -3,11 +3,10 @@ using CustomMasterDetail.Droid;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
-/*
 [assembly: ExportRenderer(typeof(MainMasterDetailPage), typeof(MainMasterDetailPageRenderer))]
 namespace CustomMasterDetail.Droid
 {
 	public class MainMasterDetailPageRenderer : MainMasterDetailRenderer
 	{
 	}
-} */
+}
